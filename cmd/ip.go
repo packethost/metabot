54c9a2879ec1635f873ab2bb6d4661275b6a9b75
Avi Deitcher <avi@deitcher.net>
2019-02-20 14:09:10 +0200

    Separate ip qualifier func for easier testing

--- cmd/ip.go
+++ cmd/ip.go
@@ -16,56 +16,60 @@ var ipCmd = &cobra.Command{
 	Short: "Print the packet ip information, subject to qualifiers",
 	Long:  `Print the packet ip information, subject to qualifiers`,
 	Run: func(cmd *cobra.Command, args []string) {
-		// ip has lots of qualifiers
-		qualifiers := map[string]bool{
-			"4":       false,
-			"6":       false,
-			"private": false,
-			"public":  false,
-			"address": false,
-			"netmask": false,
-			"parent":  false,
-		}
-		for _, a := range args {
-			qualifiers[a] = true
-		}
-		// first use the filter qualifiers to filter out which data set we want
-		addresses := make([]metadata.Address, 0)
-		for _, addr := range data.Network.Addresses {
-			// did we exclude it for a particular reason?
-			if addr.Public && qualifiers["private"] {
-				continue
-			}
-			if !addr.Public && qualifiers["public"] {
-				continue
-			}
-			if addr.AddressFamily == 6 && qualifiers["4"] {
-				continue
-			}
-			if addr.AddressFamily == 4 && qualifiers["6"] {
-				continue
-			}
-			saver := addr
-			if qualifiers["parent"] {
-				saver = *addr.Parent
-			}
-			addresses = append(addresses, saver)
-		}
-		results := make([]string, 0)
-		for _, addr := range addresses {
-			addrValue := addr.Address
-			if qualifiers["network"] {
-				addrValue = addr.Network
-			}
-			saver := fmt.Sprintf("%s/%d", addrValue, addr.Cidr)
-			switch {
-			case qualifiers["netmask"]:
-				saver = fmt.Sprintf("%s/%s", addrValue, addr.Netmask)
-			case qualifiers["address"]:
-				saver = fmt.Sprintf("%s", addrValue)
-			}
-			results = append(results, saver)
-		}
-		reportAndExit(strings.Join(results, " "))
+		reportAndExit(ipReport(args))
 	},
+}
+
+func ipReport(qualifiers []string) string {
+	// ip has lots of qualifiers
+	qualifierActive := map[string]bool{
+		"4":       false,
+		"6":       false,
+		"private": false,
+		"public":  false,
+		"address": false,
+		"netmask": false,
+		"parent":  false,
+	}
+	for _, a := range qualifiers {
+		qualifierActive[a] = true
+	}
+	// first use the filter qualifiers to filter out which data set we want
+	addresses := make([]metadata.Address, 0)
+	for _, addr := range data.Network.Addresses {
+		// did we exclude it for a particular reason?
+		if addr.Public && qualifierActive["private"] {
+			continue
+		}
+		if !addr.Public && qualifierActive["public"] {
+			continue
+		}
+		if addr.AddressFamily == 6 && qualifierActive["4"] {
+			continue
+		}
+		if addr.AddressFamily == 4 && qualifierActive["6"] {
+			continue
+		}
+		saver := addr
+		if qualifierActive["parent"] {
+			saver = *addr.Parent
+		}
+		addresses = append(addresses, saver)
+	}
+	results := make([]string, 0)
+	for _, addr := range addresses {
+		addrValue := addr.Address
+		if qualifierActive["network"] {
+			addrValue = addr.Network
+		}
+		saver := fmt.Sprintf("%s/%d", addrValue, addr.Cidr)
+		switch {
+		case qualifierActive["netmask"]:
+			saver = fmt.Sprintf("%s/%s", addrValue, addr.Netmask)
+		case qualifierActive["address"]:
+			saver = fmt.Sprintf("%s", addrValue)
+		}
+		results = append(results, saver)
+	}
+	return strings.Join(results, " ")
 }
