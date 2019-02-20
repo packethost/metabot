63bb1b319d0db9132311b8326a8a33ef147e9038
Avi Deitcher <avi@deitcher.net>
2019-02-20 15:03:42 +0200

    Add tests for IP with qualifiers

---
+++ cmd/ip_test.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"reflect"
+	"sort"
+	"strings"
+	"testing"
+	"github.com/packethost/metabot/internal"
+)
+
+func TestIpReport(t *testing.T) {
+	tests := []struct{
+		qualifiers string
+		result []string
+	}{
+		{"6 4",[]string{}},
+		{"6",[]string{"2604:1380:1:9200::1/127"}},
+		{"4", []string{"147.75.67.73/31","10.99.185.1/31"}},
+		{"",[]string{"147.75.67.73/31","10.99.185.1/31","2604:1380:1:9200::1/127"}},
+		{"address",[]string{"147.75.67.73","10.99.185.1","2604:1380:1:9200::1"}},
+		{"private",[]string{"10.99.185.1/31"}},
+		{"private 4 parent network",[]string{"10.99.185.0/25"}},
+	}
+	for i, tt := range tests {
+		result := ipReport(internal.GetValidMeta(), strings.Split(tt.qualifiers," "))
+		// sort so we can compare easily
+		sort.Strings(result)
+		sort.Strings(tt.result)
+		if !reflect.DeepEqual(result,tt.result) {
+			t.Errorf("%d %v: mismatched results, actual then expected", i, tt.qualifiers)
+			t.Logf("%s\n", result)
+			t.Logf("%s\n", tt.result)
+		}
+	}
+}
